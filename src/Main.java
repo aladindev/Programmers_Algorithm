191b8acf40dd811bdd057b314f9959118d41d327
aladindev <kjune0205@naver.com>
2022-11-05 15:48:22 +0900

    Programmers algorithm study

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		int num;
+
+		System.out.println(0%2);
 	}
 
 }
 
 
-//문제 설명
-//0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. 
-//numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 
-//return 하도록 solution 함수를 완성해주세요.
-
-class Solution {
-    public int solution(int[] numbers) {
-    	
-        int answer = 0;
-        boolean flag = true; // 배열 안에 포함되어 있는 지 여부 판단 
-        
-        for(int i = 0 ; i < 10 ; i++) {
-        	flag = true;
-        	for(int j = 0 ; j < numbers.length ; j++) {
-        		if(numbers[j] == i) {
-        			flag = false;
-        			break;
-        		}
-        	}
-        	if(flag) answer += i;
-        }
-        return answer;
-    }
-}
