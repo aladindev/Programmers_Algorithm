95aa2e3b7d6d62fb64bc7d8fc7cae8901c82a134
aladindev <kjune0205@naver.com>
2022-11-05 18:24:51 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,28 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		Solution s = new Solution();
-		
+		System.out.println(s.solution(118372));
 	}
 
 }
 
 class Solution {
-    public long[] solution(int x, int n) {
-        long[] answer = new long[n];
-        
-        for(long i = 0 ; i < n ; i++) {
-        	answer[(int) i] = x*(i+1);
+    public long solution(long n) {
+        long answer = 0;
+        char[] arr = String.valueOf(n).toCharArray();
+
+        Arrays.sort(arr);
+        String result = "";
+        for(int i = arr.length-1 ; i >= 0 ; i--) {
+        	result += arr[i];
         }
-        return answer;
+        
+        return Long.parseLong(result);
     }
 }
