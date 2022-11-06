894abda91e84da56c698962903c75eff31c96453
aladindev <kjune0205@naver.com>
2022-11-06 18:40:21 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		Solution s = new Solution();
+		s.solution("ZbcdDefg");
 	}
 
 }
@@ -15,8 +15,12 @@ class Solution {
     public String solution(String s) {
         String answer = "";
 
-        answer = s.length()%2==0 ? s.substring(s.length()/2-1, s.length()/2+1) 
-        						 : s.substring(s.length()/2, s.length()/2+1);
+        char[] arr = s.toCharArray();
+        
+        // Zbcdefg 오름차순 정렬 후 reverse
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder(String.valueOf(arr)).reverse();
+        answer = sb.toString();
         return answer;
     }
 }
