6954eba6f9db99d84d9e90794982bc2cf279e53a
aladindev <kjune0205@naver.com>
2022-11-06 18:00:11 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -7,42 +7,16 @@ public class Main {
 	public static void main(String[] args) {
 
 		Solution s = new Solution();
-		int[] test = {2, 36, 1, 3};
-		s.solution(test, 1);
-
 	}
 
 }
 
 class Solution {
-    public int[] solution(int[] arr, int divisor) {
-        int[] answer = { -1 };
+    public String solution(String s) {
+        String answer = "";
 
-        int size = 0;
-        for(int i = 0 ; i < arr.length ; i++) {
-        	if(arr[i]%divisor == 0) {
-        		size++;
-        	} else { 
-        		arr[i] = 0;
-        	}
-        }
-        
-        if(size > 0) {
-        	answer = new int[size];
-        }
-        
-        
-        for(int j = 0 ; j < answer.length ; j++) {
-        	for(int z = j ; z < arr.length ; z++) {
-        		if(arr[z] != 0) {
-        			answer[j] = arr[z];
-        			arr[z] = 0;
-        			break;
-        		}
-        	}
-        }
-        
-        if(answer != null) Arrays.sort(answer);
+        answer = s.length()%2==0 ? s.substring(s.length()/2-1, s.length()/2+1) 
+        						 : s.substring(s.length()/2, s.length()/2+1);
         return answer;
     }
 }
