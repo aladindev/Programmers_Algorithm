f0d782bc74db90d5c96564bbb913d48e65f75058
aladindev <kjune0205@naver.com>
2022-11-06 16:54:18 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,42 @@ public class Main {
 	public static void main(String[] args) {
 
 		Solution s = new Solution();
-		System.out.println( s.solution("027778888") );
-		
+		int[] test = {2, 36, 1, 3};
+		s.solution(test, 1);
+
 	}
 
 }
 
 class Solution {
-    public String solution(String phone_number) {
-        String answer = "";
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = { -1 };
+
+        int size = 0;
+        for(int i = 0 ; i < arr.length ; i++) {
+        	if(arr[i]%divisor == 0) {
+        		size++;
+        	} else { 
+        		arr[i] = 0;
+        	}
+        }
         
-        answer = phone_number.substring(0, phone_number.length()-4).replaceAll("[0-9]", "*");
-        answer += phone_number.substring(phone_number.length()-4, phone_number.length());
+        if(size > 0) {
+        	answer = new int[size];
+        }
+        
+        
+        for(int j = 0 ; j < answer.length ; j++) {
+        	for(int z = j ; z < arr.length ; z++) {
+        		if(arr[z] != 0) {
+        			answer[j] = arr[z];
+        			arr[z] = 0;
+        			break;
+        		}
+        	}
+        }
+        
+        if(answer != null) Arrays.sort(answer);
         return answer;
     }
 }
