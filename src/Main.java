ed8e1b0f1885576ae5e978d2b3891d4fcbd9689c
aladindev <kjune0205@naver.com>
2022-11-11 17:37:46 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,33 +11,43 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		Solution s = new Solution();
-		//s.solution("ZbcdDefg");
+		int[] arr = {4, 3, 2, 1};
+		s.solution(arr);
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-
-        for(int i = 0 ; i < m ; i ++) {
-            for(int j = 0 ; j < n ; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
 	}
 }
 
 class Solution {
-    public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = null;
+    public int[] solution(int[] arr) {
         
-        for(int i = 0 ; i < arr1.length ; i++) {
-        	for(int j = i ; j < arr1[i].length ; j++) {
-        		
-        	}
-        }
+    	int[] answer = null;
+    	
+    	if(arr.length == 1) {
+    		answer = new int[1];
+    		answer[0] = -1;
+    		
+    		return answer;
+    	} 
+    	answer = new int[arr.length-1];
+    	int[] tmp = arr.clone();
+    	
+    	Arrays.sort(arr);//가장 작은 수 구하기 
+    	int min = arr[0];
+    	
+    	Map<Integer, Integer> map = new HashMap<>();
+    	
+    	int keyIdx = 0;
+    	for(int i = 0 ; i < tmp.length ; i++) {
+    		if(tmp[i] != min) {
+    			map.put(keyIdx++, tmp[i]);
+    		}
+    		
+    	}
+    	for(int j = 0 ; j < answer.length ; j++) {
+    		answer[j] = map.get(j);
+    	}
+    	
         return answer;
     }
 }
