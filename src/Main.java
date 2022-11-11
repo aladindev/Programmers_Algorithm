42770afbdd2d3c9a0a206b12265b12a39d7f60da
aladindev <kjune0205@naver.com>
2022-11-11 17:53:06 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		Solution s = new Solution();
-		int[] arr = {4, 3, 2, 1};
-		s.solution(arr);
+		
 		
 		
 	}
@@ -20,34 +14,25 @@ public class Main {
 
 class Solution {
     public int[] solution(int[] arr) {
+        int[] answer = null;
         
-    	int[] answer = null;
-    	
-    	if(arr.length == 1) {
-    		answer = new int[1];
-    		answer[0] = -1;
-    		
-    		return answer;
-    	} 
-    	answer = new int[arr.length-1];
-    	int[] tmp = arr.clone();
-    	
-    	Arrays.sort(arr);//가장 작은 수 구하기 
-    	int min = arr[0];
-    	
-    	Map<Integer, Integer> map = new HashMap<>();
-    	
-    	int keyIdx = 0;
-    	for(int i = 0 ; i < tmp.length ; i++) {
-    		if(tmp[i] != min) {
-    			map.put(keyIdx++, tmp[i]);
-    		}
-    		
-    	}
-    	for(int j = 0 ; j < answer.length ; j++) {
-    		answer[j] = map.get(j);
-    	}
-    	
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+ 
+        Stack<Integer> s = new Stack<>();
+        
+        for(int i = arr.length-1 ; i > -1 ; i--) {
+        	if(i == 0) {
+        		s.push(arr[i]);
+        		break;
+        	}
+        	if(arr[i] != arr[i-1]) s.push(arr[i]);
+        }
+        
+        answer = new int[s.size()];
+        for(int j = 0 ; j < answer.length; j++) {
+        	answer[j] = s.pop();
+        }
+
         return answer;
     }
 }
