369ec0ff52a3966392d2a301d2042d5e86c0d311
aladindev <kjune0205@naver.com>
2022-11-14 16:12:54 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,25 @@ public class Main {
 
 		
 		Solution s = new Solution();
-		
+		System.out.println(s.solution("a", 4));
+		//e F d
+		// 122 + 4 = 126 
 	}
 }
 
 class Solution {
-    public int solution(int[] d, int budget) {
-        int answer = 0;
-        int sum = 0;
+    public String solution(String s, int n) {
+        char[] cArr = s.toCharArray();
         
-        Arrays.sort(d);
+        for(int i = 0 ; i < cArr.length ; i++) {
+        	if(cArr[i] == ' ') continue;
         
-        for(int i = 0 ; i < d.length ; i++) {
-        	sum+=d[i];
-        	if(sum > budget) break;
-        	answer+=1;
+        	cArr[i] += n;
+        	
+        	if( (cArr[i]-n < 91 && cArr[i] > 90) || (cArr[i]-n < 123 && cArr[i] > 122)) {
+        		cArr[i] -= 26;
+        	}
         }
-        
-        return answer;
+        return String.valueOf(cArr);
     }
 }
