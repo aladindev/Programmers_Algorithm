945da3861bfe47713a1b1d87b06eac4cf74369ca
aladindev <kjune0205@naver.com>
2022-11-14 14:55:19 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -8,44 +8,17 @@ public class Main {
 
 		
 		Solution s = new Solution();
-		//System.out.println( s.solution(" asdf asdf asdf a a a  a "));
-		String str = " asdf asdf asdf a a a  a ";
-		String[] sArr = str.split(" ", -1);
-		
-		for(String aa : sArr) {
-			System.out.println(aa);
-		}
-		
 		
 	}
 }
 
 class Solution {
-    public String solution(String s) {
-    	
-    	String[] sArr = s.split(" ", -1); // 공백 단위로 쪼개서 배열 저장 
-    	String answer = "";
-
-    	
-    	for(int j = 0 ; j < sArr.length ; j++) {
-    		//최초에는 전부 소문자로 초기화해서 캐릭터 배열에 담는다.
-    		char[] cArr = sArr[j].toLowerCase().toCharArray();
-    		
-    		for(int i = 0 ; i < cArr.length ; i++) {
-    			//짝수이면 아스키코드 -32빼서 대문자로 치환 
-    			if(i%2==0) cArr[i]-=32;
-    			
-    		}
-    		sArr[j] = String.valueOf(cArr);
-    	}
-    	
-    	for(int z = 0 ; z < sArr.length ; z++) {
-    		if(z == sArr.length-1) {
-    			answer += sArr[z];
-    		} else {
-    			answer += sArr[z] + " ";
-    		}
-    	}
+    public int solution(int n) {
+        int answer = 0;
+        
+        String notation3 = Integer.toString(n, 3); //3진법
+        String reverseStr = new StringBuilder(notation3).reverse().toString();
+        answer = Integer.parseInt(reverseStr, 3);
         
         return answer;
     }
