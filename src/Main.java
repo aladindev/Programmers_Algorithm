c8bb49be071e7d94e00e944241ded4254d4b15a6
aladindev <kjune0205@naver.com>
2022-11-26 16:11:28 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -16,27 +16,21 @@ public class Main {
 
 
 class Solution {
-    public String solution(int a, int b) {
-    	
-    	// 기준일자가 금요일이니 배열은 금요일부터 시
-    	String[] arr = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
-    	
-    	Calendar c1 = Calendar.getInstance();
-    	c1.set(2016, 0, 1);
-    	Calendar c2 = Calendar.getInstance();
-    	c2.set(2016, a-1, b);
-    	
-    	Date d1 = new Date(c1.getTimeInMillis());
-    	Date d2 = new Date(c2.getTimeInMillis());
-    	
-    	long diffMill = (d2.getTime() - d1.getTime()) / 1000;
-    	// 차이 일자 구하기 
-    	long diffDay = diffMill / (24*60*60);
-    	
-    	String answer = "";
-    	// 1주일 주기인 7일로 나눈값의 인덱스를 가져온다.
-    	answer = arr[(int) (diffDay%7)];
-    	
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        for(int i = left ; i <= right ; i++) {
+        	answer = getCnt(i)%2==0 ? answer+i : answer-i;
+        }
         return answer;
     }
+    
+   public int getCnt(int num) {
+	   int cnt = 0;
+	   
+	   for(int i = 1 ; i <= num ; i ++) {
+		   cnt = num%i==0 ? cnt+1 : cnt;
+	   }
+	   return cnt;
+   }
 }
