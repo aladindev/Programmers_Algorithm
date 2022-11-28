17a24f6e2d670bcd24a734cc3c34d42e6b6b3aef
aladindev <kjune0205@naver.com>
2022-11-28 15:33:28 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,63 @@
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		
 		Solution s = new Solution();
-		int[] arr = {1, 3, 2, 4, 2};
-		s.solution(arr);
+
+		System.out.println(s.solution(10));
+
 	}
 }
 
 class Solution {
-    public int[] solution(int[] answers) {
-    	
-    	int[] pattern1 = {1, 2, 3, 4, 5}; 				//수포자1 패턴 
-    	int[] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};		//수포자2 패턴 
-    	int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};//수포자3 패턴 
-    	
-    	int[][] result = {
-    			 {1, 0} //수포자1, 맞은개수 
-    			,{2, 0} //수포자2, 맞은개수 
-    			,{3, 0} //수포자3, 맞은개수 
-    	};
-    	
-    	for(int i = 0 ; i < answers.length ; i++) {
-    		result[0][1] = pattern1[i%5]==answers[i] ? result[0][1]+1 : result[0][1];
-    		result[1][1] = pattern2[i%8]==answers[i] ? result[1][1]+1 : result[1][1];
-    		result[2][1] = pattern3[i%10]==answers[i] ? result[2][1]+1 : result[2][1];
-    	}
-    	
-    	//맞은 개수 기준으로 내림차순 정렬 
-		Arrays.sort(result, new Comparator<int[]>() {
-		    @Override
-			public int compare(int[] o1, int[] o2) {
-		    	 return o2[1]-o1[1];
-	           }
-	        });
-		
-		int[] answer = null;
-		
-		int size = 0;
-		for(int j = 0 ; j < result.length ; j++) {
-			size = result[j][1]!=0 ? size+1 : size;
-		}
-		answer = new int[size];
-		for(int z = 0 ; z < answer.length ; z++) {
-			answer[z] = result[z][0];
-		}
-		
-        return answer;
-    }
+	public int solution(int n) { 
+	      int answer = 0; 
+	      boolean[] prime = new boolean [n+1]; 
+	      for(int i=2; i<=n ; i++) 
+	      prime[i]=true; //2~n번째수를 true로 초기화 
+	      
+	      //제곱근 구하기 
+	      int root=(int)Math.sqrt(n); 
+	      for(int i=2; i<=root; i++){ //2~루트n까지 검사 
+	      if(prime[i]==true){ //i번째의 수가 소수일 때 
+	      	 for(int j=i; i*j<=n; j++) //그 배수들을 다 false로 초기화(배수는 소수가 아니기 때문) 
+	             prime[i*j]=false; 
+	         } 
+	      } 
+	      
+	      for(int i =2; i<=n; i++) { 
+	      if(prime[i]==true)
+	         answer++; 
+	      } 
+	      return answer; 
+	  }
 }
+
+//class Solution {
+//    public int solution(int n) {
+//        int answer = 0;
+//        
+//        for(int i = n ; i > 1 ; i--) {
+//        	int a = isSosu(i);
+//        	answer += a;
+//        }
+//        
+//        return answer;
+//    }
+//    
+//    public int isSosu(int n) {
+//    	
+//    	int cnt = 2;
+//    	for(int i = (int)Math.sqrt(n) ; i > 1 ; i--) {
+//    		if(n%i == 0)  {
+//    			cnt++;
+//    			break;
+//    		}
+//    	}
+//    	
+//    	return cnt==2? 1 : 0;
+//    }
+//}
