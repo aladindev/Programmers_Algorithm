6bf004bd536a20fa447268b304eeea9f3e744e27
aladindev <kjune0205@naver.com>
2022-12-10 19:33:21 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -9,58 +9,26 @@ public class Main {
 
 		int[] a = {0,0,0,0,0,0};
 		int[] b = {38, 19, 20, 40, 15, 25};
-		s.solution(a, b);
 
 	}
 }
 
+//[1, 3, 4, 6]	"1223330333221"
+//[1, 7, 1, 2]	"111303111"
+
 class Solution {
-	
-    public int[] solution(int[] lottos, int[] win_nums) {
-    	
-    	int[] RANK_MAP = {6,6,5,4,3,2,1};
-    	
-    	int collectCnt = 0;
-    	int zeroCnt = 0;
-    	
-    	for(int i = 0 ; i < lottos.length ; i++) {
-    		for(int j = 0 ; j < win_nums.length ; j++) {
-    			collectCnt = lottos[i]==win_nums[j] ? collectCnt+1 : collectCnt;
-    		}
-    		zeroCnt = lottos[i]==0 ? zeroCnt+1 : zeroCnt;
-    	}
-    	
-        int[] answer = {RANK_MAP[collectCnt+zeroCnt], RANK_MAP[collectCnt]};
-        return answer;
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for(int i = 0 ; i < food.length ; i++) {
+        	for(int j = 0 ; j < food[i]/2 ; j++) {
+         		answer += i+1;
+        	}
+        }
+        
+        String reverse = new StringBuilder(answer).reverse().toString();
+        
+        
+        return answer + "0" + reverse;
     }
 }
-
-
-//class Solution {
-//	
-//	public static Map<Integer, Integer> RANK_MAP = Map.of(
-//					// 맞은개수, 등수
-//					6,1
-//				   ,5,2
-//				   ,4,3
-//				   ,3,4
-//				   ,2,5
-//				   ,1,6
-//				   ,0,6);
-//	
-//    public int[] solution(int[] lottos, int[] win_nums) {
-//    	
-//    	int collectCnt = 0;
-//    	int zeroCnt = 0;
-//    	
-//    	for(int i = 0 ; i < lottos.length ; i++) {
-//    		for(int j = 0 ; j < win_nums.length ; j++) {
-//    			collectCnt = lottos[i]==win_nums[j] ? collectCnt+1 : collectCnt;
-//    		}
-//    		zeroCnt = lottos[i]==0 ? zeroCnt+1 : zeroCnt;
-//    	}
-//    	
-//        int[] answer = {RANK_MAP.get(collectCnt+zeroCnt), RANK_MAP.get(collectCnt)};
-//        return answer;
-//    }
-//}
