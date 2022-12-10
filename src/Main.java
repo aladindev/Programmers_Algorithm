b075c8016386548214a253d4285f0cc09a8cff23
aladindev <kjune0205@naver.com>
2022-12-10 20:13:15 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
 
 public class Main {
@@ -7,8 +8,10 @@ public class Main {
 
 		Solution s = new Solution();
 
-		int[] a = {0,0,0,0,0,0};
-		int[] b = {38, 19, 20, 40, 15, 25};
+		String[] a = {"marina", "josipa", "nikola", "vinko", "filipa"};
+		String[] b = {"josipa", "filipa", "marina", "nikola"};
+		
+		System.out.println( s.solution(a, b));
 
 	}
 }
@@ -17,18 +20,29 @@ public class Main {
 //[1, 7, 1, 2]	"111303111"
 
 class Solution {
-    public String solution(int[] food) {
+    public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        for(int i = 0 ; i < food.length ; i++) {
-        	for(int j = 0 ; j < food[i]/2 ; j++) {
-         		answer += i+1;
-        	}
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for(String s : participant) {
+        	System.out.print(s + " ");
+        }
+        System.out.println();
+        for(String s : completion) {
+        	System.out.print(s + " ");
         }
         
-        String reverse = new StringBuilder(answer).reverse().toString();
+        for(int i = 0 ; i < completion.length ; i++) {
+        	// 참여자와 완주자가 같지 않으면 
+        	if(!participant[i].equals(completion[i])) { 
+        		answer = participant[i];
+        		break;
+        	}
+        }
+        System.out.println();
         
-        
-        return answer + "0" + reverse;
+        return "".equals(answer) ? participant[participant.length-1] : answer;
     }
 }
