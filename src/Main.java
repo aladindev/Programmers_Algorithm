379efe8ab94f8ba79d24b030cdcf7802c8fe30cc
aladindev <kjune0205@naver.com>
2022-12-25 14:49:36 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,30 @@
 import java.io.IOException;
-import java.util.Stack;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
 		Solution s = new Solution();
-		
-		int[][] b = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
-		int[] move = {1,5,3,5,1,2,1,4};
-		
-		s.solution(b, move);
 
+		int[] nums = {3,3,3,2,2,4};
 	}
 }
 
-
-/* [0,0,0,0,0]
- * [0,0,1,0,3]
- * [0,2,5,0,1]
- * [4,2,4,4,2]
- * [3,5,1,3,1]
- *  1 2 3 4 5
- *  
- * [1,5,3,5,1,2,1,4]
- * */
-
-
-// 4
-// 2
-// 4
-
 class Solution {
-    public int solution(int[][] board, int[] moves) {
-    	
-    	Stack<Integer> stack = new Stack<Integer>();
-    	int answer = 0;
-    	
-    	for(int i = 0 ; i < moves.length ; i++) {
-    		for(int j = 0 ; j < board.length ; j++) {
-    			int x = moves[i]-1;
-    			int val = board[j][x];
-    			
-    			if(val != 0) {
-    				board[j][x] = 0;
-    				if(stack.isEmpty()) {
-    					stack.push(val);
-    				} else {
-    					int bf = stack.peek();
-    					if(bf == val) {
-    						answer += 1;
-    						stack.pop();
-    					} else {
-    						stack.push(val);
-    					}
-    				}
-    				
-    				break;
-     			}
-    		}
-    	}
-        return answer*2;
+    public int solution(String t, String p) {
+        int answer = 0;
+        
+        int pLeng = p.length();
+        long pLong = Long.parseLong(p);
+        for(int i = 0 ; i < t.length() ; i++) {
+        	int idx  = i+pLeng;
+        	if(idx > t.length()) break;
+        	
+        	long l = Long.parseLong(t.substring(i, idx));
+        	answer = l<=pLong ? answer+1 : answer;
+        }
+        return answer;
     }
 }
