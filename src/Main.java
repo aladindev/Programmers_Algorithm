2fcaee40f229f9a40034eb86847168079a9c759c
aladindev <kjune0205@naver.com>
2023-01-07 18:47:02 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.Arrays;
 
 
 public class Main {
@@ -8,30 +7,34 @@ public class Main {
 	public static void main(String[] args) throws IOException, ParseException {
 
 		Solution s = new Solution();
-
-				
-		
+		s.solution("110010101001");
 	}
 }
 
 
 class Solution {
-    public String solution(String s) {
-    	String[] sArr = s.split(" ");
-        String answer = "";
-        StringBuilder sb = new StringBuilder(answer);
-
-        int[] result = new int[sArr.length];
-        int idx = 0;
-        for(String str : sArr) {
-        	result[idx++] = Integer.parseInt(str);
+    public int[] solution(String s) {
+    	
+        int[] answer = new int[2];
+        
+        int cnt = 0;
+        int delZeroCnt = 0;
+        String tmp = s;
+        
+        StringBuilder sb = new StringBuilder();
+//        while(tmp.length() > 1) {
+        while(tmp.length()>1) {
+        	cnt++;
+        	for(char c : tmp.toCharArray()) {
+            	if(c=='0') delZeroCnt ++;
+            	else sb.append("1");
+            }
+        	tmp = Integer.toString(sb.toString().length(), 2);
+        	sb.setLength(0);
         }
+        answer[0] = cnt;
+        answer[1] = delZeroCnt;
         
-        Arrays.sort(result);
-        
-        sb.append(result[0] + " ");
-        sb.append(result[result.length-1]);
-        
-        return sb.toString();
+        return answer;
     }
 }
