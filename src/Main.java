e4b9ef9f36a00885d61f0a9cf3c8a1fd8790935b
aladindev <kjune0205@naver.com>
2023-01-07 23:47:26 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -7,33 +7,26 @@ public class Main {
 	public static void main(String[] args) throws IOException, ParseException {
 
 		Solution s = new Solution();
-		s.solution("110010101001");
+		s.solution(15);
 	}
 }
 
-
 class Solution {
-    public int[] solution(String s) {
-    	
-        int[] answer = new int[2];
+    public int solution(int n) {
+        int answer = 0;
         
-        int cnt = 0;
-        int delZeroCnt = 0;
-        String tmp = s;
-        
+        String org = Integer.toString(n, 2);
         StringBuilder sb = new StringBuilder();
-//        while(tmp.length() > 1) {
-        while(tmp.length()>1) {
-        	cnt++;
-        	for(char c : tmp.toCharArray()) {
-            	if(c=='0') delZeroCnt ++;
-            	else sb.append("1");
-            }
-        	tmp = Integer.toString(sb.toString().length(), 2);
-        	sb.setLength(0);
+        
+        System.out.println(org);
+        
+        if(org.indexOf("0") < 0) {
+        	sb.append("10");
+        	sb.append(org.substring(1, org.length()));
         }
-        answer[0] = cnt;
-        answer[1] = delZeroCnt;
+        
+        System.out.println(sb.toString());
+        
         
         return answer;
     }
