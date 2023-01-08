43353e860156e739f3070babb6874e951dde985f
aladindev <kjune0205@naver.com>
2023-01-08 17:12:53 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -19,53 +19,20 @@ public class Main {
 		String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
 		int k = 2;
 		
-		s.solution(id_list, report, k);
+		s.solution(3);
 	}
 }
 
 class Solution {
-    public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
-        
-        // 신고자 별 받는 메일 개수 담는 
-        LinkedHashMap<String, Integer> result = new LinkedHashMap<>();
-        for(String s : id_list) {
-        	result.put(s,  0);
-        }
-        
-        //개인별 신고자 id를 담고 있는 Set
-        Map<String, Set<String>> map = new LinkedHashMap<>();
-
-	    for(int i = 0 ; i < id_list.length ; i++) {
-	    	map.put(id_list[i], new LinkedHashSet<>());
-	    }
-        
-        for(int i = 0 ; i < report.length ; i++) {
-        	String[] arr = report[i].split(" ");
-        	Set<String> newSet = map.get(arr[1]);
-        	
-        	if(!newSet.contains(arr[0])) {
-        		newSet.add(arr[0]);
-        	}
-        	map.put(arr[1], newSet);
-        }
-        
-        for(int i = 0 ; i < map.size() ; i++) {
-        	Set<String> s = map.get(id_list[i]);
-        	if(s.size() >= k) {
-        		Iterator<String> iter = s.iterator();
-        		while(iter.hasNext()) {
-        			String key = iter.next();
-        			result.put(key, result.getOrDefault(key, 0)+1);
-        		}
-        	}
-        }
-        
-        int idx = 0;
-        for(Entry<String, Integer> e : result.entrySet()) {
-        	answer[idx++] = e.getValue();
-        }
-        return answer;
+    public int solution(int n) {
+    	
+    	long[] arr = new long[n+2];
+    	arr[0] = 0;
+    	arr[1] = 1;
+    	for(int i = 2 ; i < arr.length ; i++) {
+    		arr[i] = arr[i-1]%1234567 + arr[i-2]%1234567;;
+    	}
+    	
+        return (int)arr[n]%1234567;
     }
 }
-
