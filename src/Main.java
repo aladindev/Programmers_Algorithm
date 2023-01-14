88e82d39fb7f969c27dc855a48ccc9bd6a8e7552
aladindev <kjune0205@naver.com>
2023-01-14 19:43:50 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -8,45 +8,26 @@ public class Main {
 	public static void main(String[] args) throws IOException, ParseException {
 
 		Solution s = new Solution();
+		String[] ba = { "ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
 
-		s.solution("baabaa");
-
-		String t = "baabaa";
-		String te = "aa";
-		System.out.println(s.solution("baabaa"));
-
+		System.out.println(s.solution(ba));
 	}
 }
-class Solution
-{
-	public int solution(String s)
-	{
-		int answer = -1;
-		char[] cArr = s.toCharArray();
-		Map<String, String> sMap = new HashMap<>();
 
-		String argTmp = s;
+class Solution {
+	public int solution(String[] babbling) {
+		int answer = 0;
+		String[] can = { "aya", "ye", "woo", "ma" };
 
-		int idx = 0;
-		for(char c : cArr) {
-			StringBuilder sb = new StringBuilder();
-			sb.append(c);
-			sb.append(c);
-
-			String tmp = sb.toString();
-			if(sMap.get(tmp) == null) {
-				sMap.put(tmp, "");
-			}
-		}
-		//baabaa
-		for(int i = 0 ; i < argTmp.length() ; i++) {
-			for(String e : sMap.keySet()) {
-				if(argTmp.indexOf(e) > -1) {
-					argTmp = argTmp.replaceAll(e, "");
+		for(String b : babbling) {
+			for(String c : can) {
+				if(b.indexOf(c) > -1) {
+					b.replaceAll(c, "");
 				}
 			}
+			answer = b.length() > 0 ? answer : ++answer;
 		}
 
-		return argTmp.length() == 0 ? 1 : 0;
+		return answer;
 	}
 }
