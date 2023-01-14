e47ea4522574d572a7bac41fbaa95ca0d8a35f0e
aladindev <kjune0205@naver.com>
2023-01-14 16:52:27 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.text.ParseException;
-
+import java.util.Arrays;
+import java.util.Comparator;
 
 
 public class Main {
@@ -8,58 +9,44 @@ public class Main {
 	public static void main(String[] args) throws IOException, ParseException {
 
 		Solution s = new Solution();
-//		String a = "3people unFollowed me";
 
-		String a = "  3peoPle";
+//		[1, 4, 2]	[5, 4, 4]	29
+//				[1,2]	[3,4]	10
 
-		s.solution(78);
+		int[] a = {1,2};
+		int[] b = {3,4};
+		System.out.println(s.solution(a,b));
+
 	}
 }
 
-// 시간초과 
-//class Solution {
-//    public int solution(int n) {
-//        int answer = 0;
-//        
-//        //1001110
-//        
-//        String n2 = Integer.toString(n, 2);
-//        n2 = n2.replaceAll("0", "");
-//        
-//        int cnt = n2.length();
-//        
-//        String tmp = "";
-//        for(int i = n+1 ; i <= 1000000 ; i++) {
-//        	tmp = Integer.toString(i, 2);
-//        	tmp = tmp.replaceAll("0", "");
-//        	
-//        	if(cnt == tmp.length()) {
-//        		answer = i;
-//        		break;
-//        	}
-//        		
-//        }
-//        return answer;
-//    }
-//}
+class Solution
+{
+	public int solution(int []A, int []B)
+	{
+		int answer = 0;
 
-//효율성통과 
-class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        
-        int cnt = Integer.bitCount(n);
-        
-        for(int i = n+1 ; i <= 1000000 ; i++) {
-        	
-        	int cnt_2 = Integer.bitCount(i);
-        	
-        	if(cnt == cnt_2) {
-        		answer = i;
-        		break;
-        	}
-        		
-        }
-        return answer;
-    }
+		Arrays.sort(A);
+		Integer[] arr = new Integer[B.length];
+
+		int idx = 0;
+		for(int i : B) {
+			arr[idx++] = i;
+		}
+		Arrays.sort(arr, new CustomComparator());
+
+		for(int i = 0 ; i < A.length ; i++) {
+			answer += A[i]*arr[i];
+		}
+		return answer;
+	}
+}
+
+class CustomComparator implements Comparator<Integer> {
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		return o2.compareTo(o1);
+	}
+
 }
