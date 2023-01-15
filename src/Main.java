7c39519c35949c98cc730b42e9d78d39f216f631
aladindev <kjune0205@naver.com>
2023-01-15 19:08:02 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,44 @@ public class Main {
 	public static void main(String[] args) throws IOException, ParseException {
 
 		Solution s = new Solution();
-
-		System.out.println( s.solution("aaba"));
-
+		s.solution("(())()");
 
 	}
 }
 
+class Solution {
+	boolean solution(String s) {
+		boolean answer = true;
+
+		Stack<Character> st = new Stack<>();
+
+		for(char c : s.toCharArray()) {
+			st.add(c);
+		}
+		int cnt = 0;
+		while(st.size() > 0) {
+
+			if(cnt == 0) {
+				if(st.peek() == '(') return false;
+				cnt++;
+				char c = st.pop();
+			}
+
+			if(st.peek() == '(') {
+				if(cnt>=st.size()) {
+					cnt = st.size();
+				}
+				for(int j = 0 ; j < cnt ; j++) {
+					char p = st.pop();
+					if(p == ')') return false;
+				}
+				cnt = 0;
+			} else {
+				cnt+=1;
+				st.pop();
+			}
+		}
+
+		return st.size() == 0 ? true : false;
+	}
+}
