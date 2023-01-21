b36193050be0f04cc122bfebea7efae64ea06d68
aladindev <kjune0205@naver.com>
2023-01-21 17:45:27 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -5,51 +5,33 @@ import java.util.*;
 
 public class Main {
 
-	public static <K> void main(String[] args) throws IOException, ParseException {
+	public static void main(String[] args) throws IOException, ParseException {
 
 		Solution s = new Solution();
-		//s.solution("(())()");
-		int[][] t = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
-		s.solution(t);
+
+		s.solution(5,3,2);
 	}
 }
 
 
 class Solution {
-	public int solution(int[][] sizes) {
+	public int solution(int number, int limit, int power) {
 		int answer = 0;
+		int[] arr = new int[number+1];
 
-		for(int i = 0 ; i < sizes.length ; i++) {
-			int tmp = 0;
-			if(sizes[i][1] > sizes[i][0]) {
-				tmp = sizes[i][0];
-				sizes[i][0] = sizes[i][1];
-				sizes[i][1] = tmp;
+		for(int i = 1 ; i < arr.length ; i++) {
+			int cnt = 0;
+			for(int j = 1 ; j*j <= i ; j++) {
+				if(j*j==i) cnt++;
+				else if(i%j==0) cnt+=2;
 			}
+			arr[i] = cnt>limit? power : cnt;
 		}
-		int xMax = 0;
-		Arrays.sort(sizes, new CustomXComparator());
-		xMax = sizes[0][0];
-		int yMax = 0;
-		Arrays.sort(sizes, new CustomYComparator());
-		yMax = sizes[0][1];
 
-		return xMax * yMax;
-	}
-}
+		for(int a : arr) {
+			answer += a;
+		}
 
-class CustomXComparator implements Comparator<int[]> {
-
-	@Override
-	public int compare(int[] o1, int[] o2) {
-		return o2[0]-o1[0];
-	}
-}
-
-class CustomYComparator implements Comparator<int[]> {
-
-	@Override
-	public int compare(int[] o1, int[] o2) {
-		return o2[1]-o1[1];
+		return answer;
 	}
 }
