8b58154757c6a8de37feaefd88c0f4829bf6dd77
aladindev <kjune0205@naver.com>
2023-01-21 15:45:31 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,47 @@ public class Main {
 
 		Solution s = new Solution();
 		//s.solution("(())()");
+		int[][] t = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+		s.solution(t);
 	}
 }
 
 
 class Solution {
-	public int solution(int[] nums) {
+	public int solution(int[][] sizes) {
 		int answer = 0;
 
-		int n2 = nums.length/2;
-
-		Set<Integer> set = new HashSet<>();
-
-		for(int n : nums) {
-			set.add(n);
+		for(int i = 0 ; i < sizes.length ; i++) {
+			int tmp = 0;
+			if(sizes[i][1] > sizes[i][0]) {
+				tmp = sizes[i][0];
+				sizes[i][0] = sizes[i][1];
+				sizes[i][1] = tmp;
+			}
 		}
+		int xMax = 0;
+		Arrays.sort(sizes, new CustomXComparator());
+		xMax = sizes[0][0];
+		int yMax = 0;
+		Arrays.sort(sizes, new CustomYComparator());
+		yMax = sizes[0][1];
 
-		return set.size()>=n2 ? n2 : set.size();
+		return xMax * yMax;
+	}
+}
+
+class CustomXComparator implements Comparator<int[]> {
+
+	@Override
+	public int compare(int[] o1, int[] o2) {
+		return o2[0]-o1[0];
+	}
+}
+
+class CustomYComparator implements Comparator<int[]> {
+
+	@Override
+	public int compare(int[] o1, int[] o2) {
+		return o2[1]-o1[1];
 	}
 }
