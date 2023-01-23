ee8d198c710319cac92425f3e0554c44abbc8565
aladindev <kjune0205@naver.com>
2023-01-23 16:49:00 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -11,28 +11,22 @@ public class Main {
 	}
 }
 
-class Solution { //
-	boolean solution(String s) {
-		boolean answer = true;
+class Solution {
+	public int solution(int n) {
+		int answer = 1;
 
-		// 문자열 첫 단어가 닫는 괄호이거나 마지막 단어가 여는 괄호이면 false
-		if(s.charAt(0) == ')' || s.charAt(s.length()-1) == '(') return false;
-
-		Stack<Character> stack = new Stack<>();
-		for(char c : s.toCharArray()) {
-			if(c == '(') {
-				stack.add(c);
-			} else if(c == ')') {
-				if(stack.isEmpty()) {
-					return false;
-				} else {
-					stack.pop();
-				}
+		for(int i = 1 ; i <= n ; i++) {
+			int sum = i;
+			for(int j = i+1 ; j <= n ; j++) {
+				sum+=j;
+				if(sum == n) {
+					answer+=1;
+					break;
+				} else if(sum > n) break;
 			}
 		}
 
-		return stack.size() > 0 ? false : true;
+		return answer;
 	}
 }
-
 
