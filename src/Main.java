c0b4004fc0ad667ca76c978b096511893bed560d
aladindev <kjune0205@naver.com>
2023-01-23 16:21:14 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -11,40 +11,68 @@ public class Main {
 	}
 }
 
-class Solution {
-	boolean solution(String s) {
-		boolean answer = true;
+class Solution
+{
+	public int solution(String s)
+	{
+		int answer = 0;
 
-		// 문자열 첫 단어가 닫는 괄호이거나 마지막 단어가 여는 괄호이면 false
-		if(s.charAt(0) == ')' || s.charAt(s.length()-1) == '(') return false;
 		Stack<Character> stack = new Stack<>();
 
-		// 스택에 담는다.
 		for(char c : s.toCharArray()) {
-			stack.add(c);
-		}
-
-		int cnt = 0; // 닫는괄호 갯수를 카운트
-		for(int i = 0 ; i < stack.size() ; i++) {
-			if(stack.peek() == ')') { // 닫는 괄호이면 갯수를 더하고 pop한다.
-				cnt++;
-				stack.pop();
-			} else if(stack.peek() == '(') { // 여는 괄호일 때
-				if(cnt > stack.size()) return false; // 닫는 괄호 갯수가 스택 사이즈 보다 크면 false(여는 괄호 갯수가 더 적은 경우)
-
-				for(int j = 0 ; j < cnt ; j++) {// 닫는 괄호 갯수만큼 pop
-					char c = stack.pop();
-					if(c == ')') return false;// pop하는 과정에서 여는 괄호가 아닌 닫는 괄호를 만나면 false(닫는 괄호 갯수가 더 많은 경우)
+			if(stack.size() > 0) {
+				if(stack.peek() == c) {
+					stack.pop();
+					answer++;
+				} else {
+					stack.add(c);
 				}
-				if(stack.size() > 0) {// 오버플로우 방지
-					if(stack.peek() == '(') return false;// 여는 괄호가 남은 경우 false(여는 괄호 갯수가 더 많은 경우)
-				}
-				cnt = 0;
-
+			} else {
+				stack.add(c);
 			}
 		}
-		return answer;
+		return stack.size() == 0 ? 1 : 0;
 	}
 }
+
+
+
+
+
+//class Solution { //
+//	boolean solution(String s) {
+//		boolean answer = true;
+//
+//		// 문자열 첫 단어가 닫는 괄호이거나 마지막 단어가 여는 괄호이면 false
+//		if(s.charAt(0) == ')' || s.charAt(s.length()-1) == '(') return false;
+//		Stack<Character> stack = new Stack<>();
+//
+//		// 스택에 담는다.
+//		for(char c : s.toCharArray()) {
+//			stack.add(c);
+//		}
+//
+//		int cnt = 0; // 닫는괄호 갯수를 카운트
+//		for(int i = 0 ; i < stack.size() ; i++) {
+//			if(stack.peek() == ')') { // 닫는 괄호이면 갯수를 더하고 pop한다.
+//				cnt++;
+//				stack.pop();
+//			} else if(stack.peek() == '(') { // 여는 괄호일 때
+//				if(cnt > stack.size()) return false; // 닫는 괄호 갯수가 스택 사이즈 보다 크면 false(여는 괄호 갯수가 더 적은 경우)
+//
+//				for(int j = 0 ; j < cnt ; j++) {// 닫는 괄호 갯수만큼 pop
+//					char c = stack.pop();
+//					if(c == ')') return false;// pop하는 과정에서 여는 괄호가 아닌 닫는 괄호를 만나면 false(닫는 괄호 갯수가 더 많은 경우)
+//				}
+//				if(stack.size() > 0) {// 오버플로우 방지
+//					if(stack.peek() == '(') return false;// 여는 괄호가 남은 경우 false(여는 괄호 갯수가 더 많은 경우)
+//				}
+//				cnt = 0;
+//
+//			}
+//		}
+//		return answer;
+//	}
+//}
 
 
