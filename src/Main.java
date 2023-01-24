34f00eb9caba1f06adcb8943e9544de85d09771c
aladindev <kjune0205@naver.com>
2023-01-24 14:01:02 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main {
 	public static void main(String[] args) throws IOException, ParseException {
 
 		Solution s = new Solution();
-		int[] a = {70, 80, 50};
+		int[] a = {50, 70, 80, 50};
 		int b = 143;
-		//s.solution(a, 100);
+		s.solution(a, 100);
 	}
 }
 
@@ -19,19 +19,30 @@ class Solution {
 	public int solution(int[] people, int limit) {
 		int answer = 0;
 
-		int u = 0, o = 0, e = 0;
-		double stdr = limit/2;
 		List<Integer> list = new ArrayList<>();
 
 		for(int p : people) {
 			list.add(p);
 		}
 
-		for(Integer i : list) {
-			if(list.contains(limit-i)) {
-				e++;
-			} 
+		for(int i = 0 ; i < list.size() ; i++) {
+			int f = list.get(i);
+
+			int sum = f;
+			for(int j = i+1 ; j < list.size() ; j++) {
+				if(f+list.get(j) == limit) {
+					answer+=1;
+					list.remove(j);
+					list.remove(i);
+					break;
+				}
+			}
 		}
+
+		for(int a : list) {
+			System.out.println(a);
+		}
+		System.out.println(answer);
 
 		return answer;
 	}
