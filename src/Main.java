bfdd4dd0f4de36ee8ecf61cfe39a57357efd47bd
aladindev <kjune0205@naver.com>
2023-02-19 16:25:41 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -8,32 +8,45 @@ public class Main {
 	public static void main(String[] args) throws IOException, ParseException {
 
 		Solution s = new Solution();
-		System.out.println(s.solution("z", "abcdefghij", 20));
-	} 
+		String[] a = {"i", "drink", "water"};
+		String[] b = {"want", "to"};
+		String[] c = {"i", "want", "to", "drink", "water"};
+
+		s.solution(b,a,c);
+	}
 }
 
 
-
 class Solution {
-	public String solution(String s, String skip, int index) {
-		char[] cArr = s.toCharArray();
-		for(int i = 0 ; i < cArr.length ; i++) {
-			int idxTmp = index;
-			char cTmp = 0;
-			for(int j = 1 ; j <= idxTmp ; j++) {
-				cTmp = (char)((int)cArr[i] + 1);
+	public String solution(String[] cards1, String[] cards2, String[] goal) {
+		String answer = "Yes";
 
-				if(cTmp > 122) {
-					int a = cTmp - 122;
-					cTmp = (char)(97 + a-1);
-				}
-				if(skip.indexOf(cTmp) > -1) {
-					idxTmp++;
-				}
-				cArr[i] = cTmp;
-			}
+		String[] start = null;
+		String[] next = null;
+		String[] result = new String[cards1.length + cards2.length];
 
+		if(goal[0].equals(cards1[0])) {
+			start = cards1.clone();
+			next = cards2.clone();
+		} else {
+			start = cards2.clone();
+			next = cards2.clone();
 		}
-		return String.valueOf(cArr);
+		result[0] = start[0];
+		int idx = 1;
+		for(int i = 0 ; i < next.length ; i++) {
+			result[idx++] = next[i];
+		}
+		for(int j = 1 ; j < start.length ; j++) {
+			result[idx++] = start[j];
+		}
+		for(int j = 0 ; j < goal.length ; j++) {
+			if(!goal[j].equals(result[j])) {
+				answer = "No";
+				break;
+			}
+		}
+
+		return answer;
 	}
 }
