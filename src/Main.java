c9c50bdc5e593c0421a487de96154da926c0127d
aladindev <kjune0205@naver.com>
2023-02-26 19:10:45 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 		String[] b = {"want", "to"};
 		String[] c = {"i", "want", "to", "drink", "water"};
 
-		s.solution(b,a,c);
+		System.out.println(s.solution(b,a,c));
 	}
 }
 
@@ -21,8 +21,37 @@ class Solution {
 	public String solution(String[] cards1, String[] cards2, String[] goal) {
 		String answer = "Yes";
 
-		
+		String[] start = null;
+		String[] next = null;
+		if(goal[0].equals(cards1[0])) {
+			start = cards1.clone();
+			next = cards2.clone();
+		} else if(goal[0].equals(cards2[0])) {
+			start = cards2.clone();
+			next = cards1.clone();
+		} else {
+			return "No";
+		}
 
+		int i = 1;
+		int j = 0;
+		int idx = 1;
+		while(idx < goal.length) {
+			String g = goal[idx++];
+			if(i < start.length) {
+				if(g.equals(start[i])) {
+					i++;
+					continue;
+				}
+			}
+			if(j < next.length) {
+				if(g.equals(next[j])) {
+					j++;
+					continue;
+				}
+			}
+			return "No";
+		}
 		return answer;
 	}
 }
