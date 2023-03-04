8654ddff7024c7e6059305013e8139b14b600dfd
aladindev <kjune0205@naver.com>
2023-03-04 15:52:15 +0900

    Programmers Alogorithm Study

--- src/Main.java
+++ src/Main.java
@@ -7,57 +7,60 @@ import java.util.Map;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws IOException, ParseException {
 
-		Solution s = new Solution();
+        Solution s = new Solution();
 
-		//["ABACD", "BCEFD"]	["ABCD","AABB"]
-		String[] a = {"ABACD", "BCEFD"};
-		String[] b = {"ABCD","AABB"};
-		s.solution(a,b);
+        //String[] a = {".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."};
+        //String[] a = {".#...", "..#..", "...#."};
+		//String[] a = {"..........", ".....#....", "......##..", "...##.....", "....#....."};
+		String[] a = {"..", "#."};
+        s.solution(a);
 
+        /*
+         *  ..........
+         *  .....#....
+         *  ......##..
+         *  ...##.....
+         *  ....#.....
+         * */
 
-	}
+    }
 }
 
 
 class Solution {
-	public int[] solution(String[] keymap, String[] targets) {
-		int[] answer = new int[targets.length];
+    public int[] solution(String[] wallpaper) {
 
-		Map<Character, Integer> map = new HashMap<>();
 
-		for(int i = 0 ; i < keymap.length ; i++) {
-			String str = keymap[i];
+        int[] answer = new int[4];
 
-			for(int j = 0 ; j < str.length() ; j++) {
-				Integer mapVal = map.get(str.charAt(j));
-				if(mapVal != null) {
-					if(mapVal > j) {
-						map.put(str.charAt(j), j+1);
+        //#가 있는 최소값과 최대값
+		int minX = wallpaper[0].length();
+		int maxX = -1;
+		int minY = 0;
+		int maxY = 0;
+
+
+        for (int i = 0; i < wallpaper.length; i++) {
+			String str = wallpaper[i];
+
+			if(str.indexOf("#") < 0) minY++;
+			else {
+				maxY = i+1;
+				for(int j = 0 ; j < str.length() ; j++) {
+					if(str.charAt(j) == '#') {
+						if(minX > j) minX = j;
+						if(maxX < j) maxX = j+1;
 					}
-				} else {
-					map.put(str.charAt(j), j+1);
 				}
 			}
 		}
 
-		for(int i = 0 ; i < targets.length ; i++) {
-			String s = targets[i];
-			char[] arr = s.toCharArray();
-
-			int cnt = 0;
-			for(int j = 0 ; j < arr.length ; j++) {
-				if(map.get(arr[j]) != null) {
-					cnt += map.get(arr[j]);
-				} else {
-					cnt = -1;
-					break;
-				}
-			}
-			answer[i] = cnt;
-		}
-
-		return answer;
-	}
+		answer[0] = minY;
+		answer[1] = minX;
+		answer[2] = maxY;
+		answer[3] = maxX;
+        return answer;
+    }
 }
