8f0fd76a94acd3e0d5d490461783165d667ff433
aladindev <kjune0205@naver.com>
2023-03-15 20:49:52 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,11 @@ public class Main {
 
         Solution s = new Solution();
 
- 
+
         //String[] a = {".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."};
         //String[] a = {".#...", "..#..", "...#."};
 		//String[] a = {"..........", ".....#....", "......##..", "...##.....", "....#....."};
-		String[] a = {"..", "#."};
+		String[] a = {"..", "##"};
         s.solution(a);
 
         /*
@@ -33,35 +33,6 @@ public class Main {
 class Solution {
     public int[] solution(String[] wallpaper) {
 
-
-        int[] answer = new int[4];
-
-        //#가 있는 최소값과 최대값
-		int minX = wallpaper[0].length();
-		int maxX = -1;
-		int minY = 0;
-		int maxY = 0;
-
-
-        for (int i = 0; i < wallpaper.length; i++) {
-			String str = wallpaper[i];
-
-			if(str.indexOf("#") < 0) minY++;
-			else {
-				maxY = i+1;
-				for(int j = 0 ; j < str.length() ; j++) {
-					if(str.charAt(j) == '#') {
-						if(minX > j) minX = j;
-						if(maxX < j) maxX = j+1;
-					}
-				}
-			}
-		}
-
-		answer[0] = minY;
-		answer[1] = minX;
-		answer[2] = maxY;
-		answer[3] = maxX;
-        return answer;
-    }
+		return null;
+	}
 }
