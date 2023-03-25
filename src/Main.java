edd19a402385539df271dfaf885857f7bdcac81a
aladindev <kjune0205@naver.com>
2023-03-25 15:22:43 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -11,28 +11,25 @@ public class Main {
 
         Solution s = new Solution();
 
-
-        //String[] a = {".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."};
-        //String[] a = {".#...", "..#..", "...#."};
-		//String[] a = {"..........", ".....#....", "......##..", "...##.....", "....#....."};
-		String[] a = {"..", "##"};
-        s.solution(a);
-
-        /*
-         *  ..........
-         *  .....#....
-         *  ......##..
-         *  ...##.....
-         *  ....#.....
-         * */
-
+        int[] g = {1, 3};
+        System.out.println(s.solution(5, 4,  g));
     }
 }
 
 
 class Solution {
-    public int[] solution(String[] wallpaper) {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
 
-		return null;
-	}
+        for(int i = section.length-1 ; i>=0 ; i--) {
+            if(i>0) {
+                int diff = section[i]-section[i-1];
+
+                if(m > diff) i--;
+            }
+            answer++;
+        }
+
+        return answer;
+    }
 }
