91936b71e8c60877590a346f44152766f8a51400
aladindev <kjune0205@naver.com>
2023-08-11 16:34:56 +0900

    Programmers Algorithm Study

--- src/GenericStudy.java
+++ src/GenericStudy.java
@@ -1,12 +1,21 @@
 
 /**
+ *  Part 1
  *  인스턴스 생성 시 결정이 되는 자료형의 정보를 T로 대체한다.
  *  Type Parameter(타입 매개변수) : GenericStudy<T>에서 T
  *  Type Argument(타입 인자)     : GenericStudy<instance>에서 instance
  *  Parameterized Type(매개변수화 타입) : GenericStudy<instance>
  *  (컴파일러는 매개변수화 타입 '자체'를 하나의 별도의 자료형으로 판단한다.)
  *
+ *  제네릭의 장점 : 코드의 안전성을 높힌다
+ *  ? 제네릭을 사용하지 않으면 프로그래머가 직접 명시적 형변환을 해야한다.
+ *  > 이는 컴파일러의 개입을 허용하지 않기에 컴파일 단계에서 에러가 발생하지 않는다.
+ *  > 제네릭을 사용함으로써 매개변수화 타입을 결정하면 타입 인자가 결정된 타입 매개변수만을
+ *    받기 때문에 컴파일 단계에서 에러를 검출할 수 있다.
+ *
  * */
+
+/* part 1
 public class GenericStudy<T> {
     private T object;
 
@@ -17,5 +26,24 @@ public class GenericStudy<T> {
     public T get() {
         return object;
     }
+}
+*/
 
+/** part 2
+ * Part 2 !!!! 제네릭 클래스의 타입인자(Type Argument) 제한하기
+ *  >> 인스턴스 생성 시 타입 인자(Type Argument)로 Number 또는
+ *  이를 상속하는 클래스만 올 수 있게 제한하는 것
+ *
+ * */
+
+public class GenericStudy<T extends Number> {
+    private T object;
+
+    public void set(T o) {
+        object = o;
+    }
+
+    public T get() {
+        return object;
+    }
 }
