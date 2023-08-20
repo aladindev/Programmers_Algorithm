58449a4704b6f912d20247cbb07d491b374843e7
aladindev <kjune0205@naver.com>
2023-08-20 17:35:18 +0900

    Programmers Algorithm Study

--- src/OptionalClassStudy.java
+++ src/OptionalClassStudy.java
@@ -14,7 +14,14 @@
  *              ifPresent(s -> System.out.println(s));
  *              ifPresent(System.out::println);
  *          .map() : 매개변수를 전달받은 뒤 람다식을 수행한 값을 반환!!
- *                   반드시!! input -> s.toLowerCase()); -> input.toLowerCase() 반환 
+ *                   반드시!! input -> s.toLowerCase()); -> input.toLowerCase() 반환
+ *                   반환형은 Optional<T>형으로 반환한다!!
+ *                   Optional<String> os = os.map(s -> s.replace(' ', '_')).map(s -> s.toLowerCase()));
+ *                   os.get();
+ *          .flatMap : map과 다르게 반환을 Optional<T> 인스턴스를 반환하지 않고
+ *                     수행 결과를 그대로(Optional로) 감싸지 않고 반환.
+ *          .orElse : Optional<T> 인스턴스로 반환하는 map()과 달리
+ *                    인스턴스가 null이면 인스턴스 내 실제 Type value를 반환하는 메소드!!
  * */
 
 
