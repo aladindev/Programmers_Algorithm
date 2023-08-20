baee382a0799950969903bc117b1a16fb7296a7f
aladindev <kjune0205@naver.com>
2023-08-20 14:28:07 +0900

    Programmers Algorithm Study

--- src/LambdaStudy.java
+++ src/LambdaStudy.java
@@ -46,11 +46,21 @@
  *  함수형 인터페이스 : 추상 메소드가 딱 하나(구현해야 할 메소드가 딱 하나)만 존재하는 인터페이스
  *  @FunctionalInterface : 함수형 인터페이스의 조건을 갖추었는지에 대한 검사를 컴파일러에게 요청!!
  *  @FunctionalInterface
- *  interface Test { >> 추상 메소드가 하나이므로 함수형 인터페이스 조건에 부합 
+ *  interface Test { >> 추상 메소드가 하나이므로 함수형 인터페이스 조건에 부합
  *      int c(int a, int b) >> 추상메소드이므로 구현해야함
  *      default method() {..} >> default 메소드 구현 필요없음
  *      static int staticMethod() {...} >> static 메소드 구현 필요 없음
  *  }
  *
+ *  람다식과 제네릭
+ * @FunctionalInterface
+ * interface Cal<T> { // 제네릭 기반의 함수형 인터페이스
+ *     T cal(T a, T b);
+ * }
  *
+ * Cal<Integer> c = (a, b) -> a+b;
+ * 인터페이스가 제네릭 기반이라하여 특별히 신경 쓸 부분은 없다.
+ * 타입 인자가 전달이 되면(결정이 되면) 추상 메소드의 T는 결정이 되므로!
+ *
+ * 미리 정의되어 있는 함수형 인터페이스 
  * */
---
+++ src/MethodReferencesStudy.java
@@ -0,0 +1,12 @@
+/**
+ *  Method References 4가지 유형과 Method References의 장점
+ *  Method References : 기본적으로 람다식보다 조금 더 코드를 단순하게 할 수 있다는 장점
+ *                      일부 람다식을 메소드 참조로 대신하게 할 수 있다.(전부는 아님)
+ *  - static method references
+ *
+ *  - 참조변수를 통한 인스턴스 메소드 참조
+ *
+ *  - 클래스 이름을 통한 인스턴스 메소드 참조
+ *
+ *  - 생성자 참조
+ * */
