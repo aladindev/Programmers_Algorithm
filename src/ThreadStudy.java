a31d3d14a91b47ac7f19d67d9d692fcc505401dd
aladindev <kjune0205@naver.com>
2023-09-02 15:31:15 +0900

    Programmers Algorithm Study

--- src/ThreadStudy.java
+++ src/ThreadStudy.java
@@ -5,4 +5,18 @@
  *  Process : 실행중인 프로그램
  *  하나의 프로세스 안에서 여러 프로그램의 흐름을 가진다.
  *  쓰레드 >> 프로세스에 있어서 프로그램의 흐름을 형성하는 최소 단위!!
+ *
+ *  Thread ct = Thread.currentThread()
+ *  : 현재 코드가 실행되고 있는 인스턴스의 참조 값을 반환
+ *  ex) Main Thread 내에서 위 인스턴스를 참조하면
+ *      Main 쓰레드의 정보에 접근할 수 있다.
+ *      ct.getName() :: main
+ *
+ *  쓰레드 생성
+ *  Runnable task = () -> { // 스레드가 실행하게 될 내용
+ *
+ *
+ *  };
+ *  Thread t = new Thread(task);
+ *  t.start();
  * */
