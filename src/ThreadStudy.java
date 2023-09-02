61b98edf993087c6dff884d8c36bd4adcd24c559
aladindev <kjune0205@naver.com>
2023-09-02 16:07:33 +0900

    Programmers Algorithm Study

--- src/ThreadStudy.java
+++ src/ThreadStudy.java
@@ -19,4 +19,19 @@
  *  };
  *  Thread t = new Thread(task);
  *  t.start();
+ *
+ *  t.join(); 실행중인 스레드가 종료될 때까지 기다리겠다.
+ *
+ *  멀티쓰레드의 동일한 메모리 공간에 접근하면
+ *  코어 할당으로 인해서 제대로 된 결과를 만들어 낼 수 없다.
+ *  해결책 : 스레드의 동기화 !!(스레드가 작업중일때 코어 할당을 막는다.)
+ *
+ *  동기화 방법
+ *  1. 동기화 메소드(synchronized)
+ *  synchronized method()
+ *
+ *  2. 동기화 블록
+ *  method() {
+ *      synchronized(this)
+ *  }
  * */
