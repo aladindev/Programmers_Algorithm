1dc0a53e2a3d13a4de9cac2b51c1b2a165a66787
aladindev <kjune0205@naver.com>
2023-09-02 15:21:53 +0900

    Programmers Algorithm Study

--- src/FileSystemStudy.java
+++ src/FileSystemStudy.java
@@ -9,4 +9,14 @@
  *
  *  Path path = Paths.get("") // 현재 디렉토리 정보 '상대 경로' 형태로 담긴 인스턴스 생성
  *  boolean path.isAbsolute()
+ *  path.toAbsolutePath() >> 절대 경로로 변환
+ *
+ *  public static Path createFile(Path path, FileAttribute<?> ...attrs) throws IOException
+ *  > 지정한 경로에 빈 파일 생성, 경로가 유효하지 않거나 파일이 존재하면 예외 발생
+ *
+ *  public static Path createDirectory(Path dir, FileAttribute<?> ...attrs) throws IOException
+ *  > 지정한 경로에 디렉토리 생성, 경로가 유효하지 않으면 예외 발생
+ *
+ *  public static Path createDirectories(Path dir, FileAttribute<?> ...attrs) throws IOException
+ *  > 지정한 경로의 모든 디렉토리 생성
  * */
