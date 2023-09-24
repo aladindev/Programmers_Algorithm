fb595c8795358f659471ba5ae3a504f90a6cc7e9
aladindev <kjune0205@naver.com>
2023-09-24 15:34:19 +0900

    Programmers Algorithm Study

--- src/Level2/귤고르기.java
+++ src/Level2/귤고르기.java
@@ -30,68 +30,32 @@ k	tangerine	result
 경화는 크기가 1인 귤 2개를 판매하거나 2인 귤 2개를 판매할 수 있습니다. 이때의 크기 종류는 1가지로, 이 값이 최소가 됩니다.
  * */
 
+import java.util.*;
 class 귤고르기 {
-    public String solution(String s) {
-        String answer = "";
-        StringBuilder sb = new StringBuilder(answer);
-        
-        
-        //  3peoPle
-        for(int i = 0 ; i < s.length() ; i++) {
-        	char c = s.charAt(i);
-        	
-        	if((int)c == (int)' ') {
-        		sb.append(',');
-        		if(i+1 < s.length()) {
-        			if((int)s.charAt(i+1) == (int)' ') {
-        				sb.append(' ');
-        				i+=1;
-        			}
-        		}
-         	} else {
-        		sb.append(c);
-        	}
-        	
-        }
-        
-        String[] strArr = sb.toString().replaceAll(",", ", ").split(",");
-        sb.setLength(0);
-        
-        
-        for(int i = 0 ; i < strArr.length ; i++) {
-        	String str = strArr[i];
-        	
-        	char c = ' ';
-        	if(str.length()>0) {
-        		c = str.charAt(0);
-        	} 
-        	
-        	if(c >= (int)'0' && c<= (int)'9') {
-        		sb.append(c);
-        		if(str.length() > 1) {
-        			sb.append(str.substring(1, str.length()).toLowerCase());	
-        		}
-        	} else if((int)c == ' ') {
-        		String a = str.replaceAll("[a-zA-Z0-9]", ""); //순수 공백 
-        		String b = str.replace(" ", ""); //순수 문자열 
-        		
-        		sb.append(a);
-        		if(b.length() > 0) {
-        			sb.append(String.valueOf(b.charAt(0)).toUpperCase());
-        			if(b.length() > 1) {
-        				sb.append(b.substring(1, b.length()).toLowerCase());
-        			}
-        		}
-        		
-        	} else {
-        		sb.append(String.valueOf(c).toUpperCase());
-        		
-        		if(str.length() > 1) {
-        			sb.append(str.substring(1, str.length()).toLowerCase());
-        		}
-        	}
-        }
-        
-        return sb.toString();
-    }
+	public int solution(int k, int[] tangerine) {
+		int answer = 0;
+		int[] copy = new int[10000001];
+
+		for(int i : tangerine) {
+			copy[i] += 1;
+		}
+
+		Integer[] copyB = Arrays.stream(copy).boxed().toArray(Integer[]::new);
+		Arrays.sort(copyB, (o1, o2) -> {return o2 - o1;});
+
+		int cnt = 0;
+		while(cnt < k) {
+			int ele = copyB[answer];
+			if(cnt+ele > k) {
+				answer++;
+				break;
+			} else {
+				cnt += ele;
+				if(ele != 0) {
+					answer++;
+				}
+			}
+		}
+		return answer;
+	}
 }
