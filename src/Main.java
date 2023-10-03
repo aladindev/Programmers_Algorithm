4071648ad448e0c0e26f5908f257335d64482260
aladindev <kjune0205@naver.com>
2023-10-03 14:38:03 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -31,9 +31,48 @@ class Solution {
             }
         }
 
-        System.out.println(result);
+        List<Map<Integer, Integer>> resultList = new ArrayList<>();
 
-        return answer;
+        for(int i = 0 ; i < result.size() ; i++) {
+            List<Integer> keyList = new ArrayList<>(result.get(i).keySet());
+            Map<Integer, Integer> map = new LinkedHashMap<>();
+            int finalI = i;
+            Collections.sort(keyList, (o1, o2) -> {return result.get(finalI).get(o2) - result.get(finalI).get(o1);});
+
+            for(int k : keyList) {
+                map.put(k, result.get(i).get(k));
+            }
+            resultList.add(map);
+        }
+
+        Set<Integer> pick = new HashSet<>();
+        int max = 0;
+        int qlry = 0;
+
+
+        /**
+         {4=100, 3=70, 0=40, 2=30, 1=20}
+         {4=100, 2=30, 0=10, 1=5 , 3=0 }
+         {4=100, 3=70, 2=30, 0=10, 1=0 }
+         * */
+
+        for(Map<Integer, Integer> map : resultList) {
+            Iterator<Map.Entry<Integer, Integer>> entry = map.entrySet().iterator();
+            while(entry.hasNext()) {
+                Map.Entry<Integer, Integer> next = entry.next();
+
+                int key = next.getKey();
+                int value = next.getValue();
+                if(pick.contains(key)) continue;
+
+                qlry += value;
+                pick.add(key);
+                break;
+            }
+            max = qlry > max ? qlry : max;
+        }
+
+        return max;
     }
 }
 
