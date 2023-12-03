d123c05c5ee17c0319784a80ef763c0388e7e770
aladindev <kjune0205@naver.com>
2023-12-03 14:13:55 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -12,94 +12,63 @@ public class Main {
 
         Solution s = new Solution();
 
-        s.solution(new int[][] {{10, 20, 30}, {40, 50, 60}, {70, 80, 90}});
+        int[][] data = {
+                /*  0      1        2       3 */
+                {1, 20300104, 100, 80},
+                {2, 20300804, 847, 37},
+                {3, 20300401, 10, 8},
+        };
 
+        int[][] result = s.solution(data, "date", 20300501, "remain");
+
+        for(int i = 0 ; i < result.length ; i++) {
+            for(int j = 0 ; j < result[i].length ; j++) {
+                System.out.print(result[i][j] + " / ");
+            }
+            System.out.println();
+        }
     }
 }
 
 class Solution {
-    public int solution(int[][] ability) {
-        int answer = 0;
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        //code	date	maximum	remain
 
-        // 과목, 사람별 점수맵
-        Map<Integer, Map<Integer, Integer>> result = new TreeMap<>();
-        for(int i = 0 ; i < ability.length ; i++) { // 행 : 사람
-            for(int j = 0 ; j < ability[i].length ; j++) { // 열 : 과목
-                Map<Integer, Integer> tmp = result.getOrDefault(j, new TreeMap<>());
-                tmp.put(i, ability[i][j]);
-                result.put(j, tmp);
+        Map<String, Integer> idxMap = new HashMap<>() {
+            {
+                put("code",    0);
+                put("date",    1);
+                put("maximum", 2);
+                put("remain" , 3);
+            }
+        };
+
+        Map<Integer, int[]> map = new HashMap<>();
+        List<int[]> list = new ArrayList<>();
+
+        for(int i = 0 ; i < data.length ; i++) {
+            if(data[i][idxMap.get(ext)] < val_ext) {
+                //map.put(i, data[i]);
+                list.add(data[i]);
             }
         }
-
-        List<Map<Integer, Integer>> resultList = new ArrayList<>();
-        //TODO
-        List<List<Integer>> sortKeyList = new ArrayList<>();
-
-        for(int i = 0 ; i < result.size() ; i++) {
-            List<Integer> keyList = new ArrayList<>(result.get(i).keySet());
-            Map<Integer, Integer> map = new LinkedHashMap<>();
-            int finalI = i;
-            Collections.sort(keyList, (o1, o2) -> {return result.get(finalI).get(o2) - result.get(finalI).get(o1);});
-            sortKeyList.add(keyList);
-
-            for(int k : keyList) {
-                map.put(k, result.get(i).get(k));
+        Collections.sort(list, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[idxMap.get(sort_by)] - o2[idxMap.get(sort_by)];
             }
-            resultList.add(map);
+        });
+        int[][] answer = new int[list.size()][idxMap.size()];
+        for(int i = 0 ; i < list.size() ; i++) {
+            answer[i] = list.get(i);
         }
 
-
-        /**
-         {4=100, 3=70, 0=40, 2=30, 1=20}
-         {4=100, 2=30, 0=10, 1=5 , 3=0 }
-         {4=100, 3=70, 2=30, 0=10, 1=0 }
-
-         sortKeyList
-         [4, 3, 0, 2, 1]
-         [4, 2, 0, 1, 3]
-         [4, 3, 2, 0, 1]
-
-
-         {0=10, 1=40, 2=70}
-         {0=20, 1=50, 2=80}
-         {0=30, 1=60, 2=90}
-         [2, 1, 0]
-         [2, 1, 0]
-         [2, 1, 0]
-         * */
-
-
-        System.out.println(result);
-        System.out.println(sortKeyList);
-
-        int max = 0;
-        for(int i = 0 ; i < ability.length ; i++) { // 첫 항의 기준이 되는 사람수만큼 반복 5번
-            Set<Integer> pick = new HashSet<>();
-            int qlry = 0;
-            int firstKey = sortKeyList.get(0).get(i);
-            pick.add(firstKey);
-            qlry += result.get(0).get(firstKey);
-
-            for(int j = 1 ; j < sortKeyList.size() ; j++) {
-                int nextKey = sortKeyList.get(j).get(0);
-                if(pick.contains(nextKey)) {
-                    for(int k = 0 ; k < ability.length ; k++) {
-                        int findKey = sortKeyList.get(j).get(k);
-                        if(!pick.contains(findKey)) {
-                            nextKey = findKey;
-                            break;
-                        }
-                    }
-                }
-                int value = result.get(j).get(nextKey);
-                qlry += value;
-                pick.add(nextKey);
-            }
-            max = Math.max(max, qlry);
-        }
-        return max;
+        return answer;
     }
 }
+
+
+
 class Box<T> {
     private T ob;
     public void set(T o) {
