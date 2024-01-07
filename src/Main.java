9fbc376d017d2a1b0e2113f6ba7191c248655d95
aladindev <kjune0205@naver.com>
2024-01-07 20:38:42 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -15,22 +15,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] line1 = br.readLine().split(" ");
 
-        TreeSet<String> set = new TreeSet<>();
-        for(int i = 0 ; i < Integer.parseInt(line1[0]) ; i++) {
-            String[] tmp = br.readLine().split(" ");
-            if("enter".equals(tmp[1])) set.add(tmp[0]);
-            else if("leave".equals(tmp[1])) set.remove(tmp[0]);
+        Set<String> set1 = new TreeSet<>();
+        Set<String> set2 = new HashSet<>();
+        for(int i = 0 ; i < Integer.parseInt(line1[0]) ; i++) set1.add(br.readLine());
+        for(int i = 0 ; i < Integer.parseInt(line1[1]) ; i++) set2.add(br.readLine());
+
+        Set<String> result = new TreeSet<>();
+
+        int large = Integer.parseInt(line1[0]) > Integer.parseInt(line1[1]) ? Integer.parseInt(line1[0]) : Integer.parseInt(line1[1]);
+        Set<String> largeClone = set2.size() > set1.size() ? set2 : set1;
+        Set<String> smallClone = set2.size() > set1.size() ? set1 : set2;
+        for(String s : largeClone) {
+            if(smallClone.contains(s)) result.add(s);
         }
-
-        List<String> toList = new ArrayList<>(set);
-        Collections.sort(toList, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o2.compareTo(o1);
-            }
-        });
-
-        for(String s : toList) System.out.println(s);
+        System.out.println(result.size());
+        for(String s : result) System.out.println(s);
     }
 }
 
