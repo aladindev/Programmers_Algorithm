242823ecb1119f28fa50442f0cbdf66e2e6e8de6
aladindev <kjune0205@naver.com>
2024-01-14 15:03:15 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -11,25 +11,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 
-        //백준 1764
+        //백준 1269
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] line1 = br.readLine().split(" ");
+        int a = Integer.parseInt(line1[0]);
+        int b = Integer.parseInt(line1[1]);
 
-        Set<String> set1 = new TreeSet<>();
+        String[] line3 = br.readLine().split(" ");
+        Set<String> set1 = new HashSet<>();
+        for(String s : line3) set1.add(s);
+
+        String[] line4 = br.readLine().split(" ");
         Set<String> set2 = new HashSet<>();
-        for(int i = 0 ; i < Integer.parseInt(line1[0]) ; i++) set1.add(br.readLine());
-        for(int i = 0 ; i < Integer.parseInt(line1[1]) ; i++) set2.add(br.readLine());
+        for(String s : line4) set2.add(s);
 
-        Set<String> result = new TreeSet<>();
+        int result = 0;
 
-        int large = Integer.parseInt(line1[0]) > Integer.parseInt(line1[1]) ? Integer.parseInt(line1[0]) : Integer.parseInt(line1[1]);
-        Set<String> largeClone = set2.size() > set1.size() ? set2 : set1;
-        Set<String> smallClone = set2.size() > set1.size() ? set1 : set2;
-        for(String s : largeClone) {
-            if(smallClone.contains(s)) result.add(s);
+        Iterator<String> iter1 = set1.iterator();
+        while(iter1.hasNext()) {
+            String s = iter1.next();
+            if(!set2.contains(s)) result++;
         }
-        System.out.println(result.size());
-        for(String s : result) System.out.println(s);
+
+        Iterator<String> iter2 = set2.iterator();
+        while(iter2.hasNext()) {
+            String s = iter2.next();
+            if(!set1.contains(s)) result++;
+        }
+
+        System.out.println(result);
     }
 }
 
