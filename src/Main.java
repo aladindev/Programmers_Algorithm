e25a1622d3a2663cc8bc29e3c139dd3db60a882b
aladindev <kjune0205@naver.com>
2024-01-20 21:24:27 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -11,35 +11,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 
-        //백준 1269
+        //백준 11478
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line1 = br.readLine().split(" ");
-        int a = Integer.parseInt(line1[0]);
-        int b = Integer.parseInt(line1[1]);
-
-        String[] line3 = br.readLine().split(" ");
-        Set<String> set1 = new HashSet<>();
-        for(String s : line3) set1.add(s);
-
-        String[] line4 = br.readLine().split(" ");
-        Set<String> set2 = new HashSet<>();
-        for(String s : line4) set2.add(s);
-
-        int result = 0;
-
-        Iterator<String> iter1 = set1.iterator();
-        while(iter1.hasNext()) {
-            String s = iter1.next();
-            if(!set2.contains(s)) result++;
-        }
-
-        Iterator<String> iter2 = set2.iterator();
-        while(iter2.hasNext()) {
-            String s = iter2.next();
-            if(!set1.contains(s)) result++;
-        }
-
-        System.out.println(result);
+        String line1 = br.readLine();
+        
     }
 }
 
