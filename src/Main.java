4d3698d2730fd2ae47a76c9f3aba5053a3f66782
aladindev <kjune0205@naver.com>
2024-01-28 16:44:33 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 
-        //백준 11478 ㄹ대ㅑ저햐ㅐ더햐ㅐㄷ저햐ㅐㄷ저햐ㅐ
+        //백준 11478
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line1 = br.readLine();
 
