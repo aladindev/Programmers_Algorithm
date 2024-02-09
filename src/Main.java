82fe0100769002ee3cef9ef1838e3eae1184656c
aladindev <kjune0205@naver.com>
2024-02-09 19:38:44 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 
 
-public class Main { 
+public class Main {
 
     public static void main(String[] args) throws IOException {
 
@@ -15,21 +15,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line1 = br.readLine();
 
-        Set<String> set = new HashSet<>();
-        for(int i = 0 ; i < line1.length() ; i++) {
-            for(int j = i ; j <= line1.length() ; j++) {
-                String s = line1.substring(i, j);
-                if(s.length() > 0) set.add(line1.substring(i, j));
-            }
-        }
-        System.out.println(set.size());
     }
 }
 
 class Solution {
-    public int[] solution(String[] park, String[] routes) {
-
-        return null;
-
-    }
+    
 }
