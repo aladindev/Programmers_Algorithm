196c390b198263a0a0630bd058a5c6806a200272
aladindev <kjune0205@naver.com>
2024-02-11 14:18:21 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,10 @@ public class Main {
         //Llama2 Fine Tuning (LLM, Large Language Model)
         // Prompt Tuning
         // Llama2를 KorQuad 데이터셋에 맞게 Fine-Tuning 하기
+        // KorQuad 데이터셋에 Fine-Tuning & Fine-Tuning된 GPT 예측
+        // 좁은 범위의 데이터를 Fine-Tuning을 통해 직접 학습 시키면
+        // 정확한 답은 도출할 수 있으며, 학습 시킨 범위에 한에서는 가장 성능 좋은
+        // AI가 될 수 있다.
 
     }
 }
