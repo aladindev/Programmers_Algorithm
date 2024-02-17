71f401d806dabe1a5db7997247e6ca01b7f6bfde
aladindev <kjune0205@naver.com>
2024-02-17 19:19:17 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,28 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
 
-        // 딥러닝 머신러닝 인공지능 fine tuning 실습 
+        //
+        // 딥러닝 머신러닝 인공지능 fine tuning 실습
         //백준 / Programmers 알고리즘
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line1 = br.readLine();
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //String line1 = br.readLine();
 
-        //Llama2 Fine Tuning (LLM, Large Language Model)
-        // Prompt Tuning
-        // Llama2를 KorQuad 데이터셋에 맞게 Fine-Tuning 하기
-        // KorQuad 데이터셋에 Fine-Tuning & Fine-Tuning된 GPT 예측
-        // 좁은 범위의 데이터를 Fine-Tuning을 통해 직접 학습 시키면
-        // 정확한 답은 도출할 수 있으며, 학습 시킨 범위에 한에서는 가장 성능 좋은
-        // AI가 될 수 있다. 
+        //[[1, 8, 1, 13], [5, 18, 6, 9]], 10000
+        int[] a = {1, 8, 1, 13};
+        int[] b = {5, 18, 6, 9};
+        int[][] ab = {a, b};
+        Solution s = new Solution();
 
+    }
+
+}
+class Solution {
+    public String solution() throws Exception {
+
+        return null;
     }
 }
 
-class Solution {
-
-}
