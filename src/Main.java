5e6be1a2615c7fe544ec6d31101206ee54c5e36a
aladindev <kjune0205@naver.com>
2024-02-17 19:19:41 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,9 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line1 = br.readLine();
 
-        //
-        // 딥러닝 머신러닝 인공지능 fine tuning 실습
-        //백준 / Programmers 알고리즘
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //String line1 = br.readLine();
-
-        //[[1, 8, 1, 13], [5, 18, 6, 9]], 10000
         int[] a = {1, 8, 1, 13};
         int[] b = {5, 18, 6, 9};
         int[][] ab = {a, b};
