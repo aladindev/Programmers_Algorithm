24bf96f37ac9f2c4fa685ebcc6d2b11ad36383b1
aladindev <kjune0205@naver.com>
2024-02-18 15:40:50 +0900

    Programmers Algorithm Study

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line1 = br.readLine();
+        String line1 = br.readLine();  
 
         int[] a = {1, 8, 1, 13};
         int[] b = {5, 18, 6, 9};
